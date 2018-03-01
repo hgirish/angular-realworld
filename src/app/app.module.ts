752c52d7cf82cc15c841084c84d2b9ceee621541
hgirish <girish@post.com>
2018-02-28 19:30:27 -0800

    Basic Page Layout and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule, ModuleWithProviders } from '@angular/core'
+import { RouterModule } from '@angular/router'
 
+import { AppComponent } from './app.component'
+import { SharedModule, FooterComponent, HeaderComponent } from './shared'
+import { HomeModule } from './home/home.module'
 
-import { AppComponent } from './app.component';
-
-
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], {
+  useHash: true
+})
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  imports: [BrowserModule, SharedModule, HomeModule, rootRouting],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, ModuleWithProviders } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { HomeComponent } from './home.component'
+import { SharedModule } from '../shared'
+
+const homeRouting: ModuleWithProviders = RouterModule.forChild([
+  {
+    path: '',
+    component: HomeComponent
+  }
+])
+@NgModule({
+  imports: [homeRouting, SharedModule],
+  declarations: [HomeComponent]
+})
+export class HomeModule {}
