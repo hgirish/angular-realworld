1f4bf59fe4840fcace2ebd87613b9e8749b07b4a
hgirish <girish@post.com>
2018-03-03 16:49:10 -0800

    folowing and unfollowing profiles

--- src/app/shared/services/profiles.service.ts
+++ src/app/shared/services/profiles.service.ts
@@ -15,4 +15,12 @@ export class ProfilesService {
       .get('/profiles/' + username)
       .map((data: { profile: Profile }) => data.profile)
   }
+
+  follow(username: string): Observable<Profile> {
+    return this.apiService.post('/profiles/' + username + '/follow')
+  }
+
+  unfollow(username: string): Observable<Profile> {
+    return this.apiService.delete('/profiles/' + username + '/follow')
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http'
 import { RouterModule } from '@angular/router'
 import { ListErrorsComponent } from './list-errors.component'
 import { ShowAuthedDirective } from './show-authed.directive'
+import { FollowButtonComponent } from './buttons/follow-button.component'
 
 @NgModule({
   imports: [
@@ -14,7 +15,11 @@ import { ShowAuthedDirective } from './show-authed.directive'
     HttpModule,
     RouterModule
   ],
-  declarations: [ListErrorsComponent, ShowAuthedDirective],
+  declarations: [
+    ListErrorsComponent,
+    ShowAuthedDirective,
+    FollowButtonComponent
+  ],
   exports: [
     CommonModule,
     FormsModule,
@@ -22,7 +27,8 @@ import { ShowAuthedDirective } from './show-authed.directive'
     HttpModule,
     RouterModule,
     ListErrorsComponent,
-    ShowAuthedDirective
+    ShowAuthedDirective,
+    FollowButtonComponent
   ]
 })
 export class SharedModule {}
